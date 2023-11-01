acddb9cdae87026ddc9ac065ce5d62eb5af986a1
Imad Moukadem <imad.moukadem@mu.edu.lb>
2023-11-01 08:28:09 -0700

    First version

---
+++ Web/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+public class CarsController : Controller
+{
+    public List<string> Cars { get; set; } = new();
+
+    public CarsController()
+    {
+        Cars.Add("Ferrari");
+        Cars.Add("BMW");
+        Cars.Add("Mercedes");
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Cars = Cars;
+        return View();
+    }
+}
