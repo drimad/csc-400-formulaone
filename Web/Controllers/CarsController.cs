9cb2f9f03e3a140b1b128a9d5b4a4d28a6044031
Imad Moukadem <imad.moukadem@mu.edu.lb>
2023-11-13 07:52:44 -0800

    Completed Add/Delete functionalities fixed icons

--- Web/Controllers/CarsController.cs
+++ Web/Controllers/CarsController.cs
@@ -4,25 +4,51 @@ namespace Web.Controllers;
 
 public class CarsController : Controller
 {
-    public static List<string> Cars { get; set; } = new();
-
-    public CarsController()
+    private static List<string> Cars { get; set; } = new()
     {
-        Cars.Add("Ferrari");
-        Cars.Add("BMW");
-        Cars.Add("Mercedes");
-    }
+        "Ferrari", "BMW", "Mercedes"
+    };
 
+    [HttpGet]
     public IActionResult Index()
     {
         ViewBag.Cars = Cars;
         return View();
     }
 
+    [HttpGet]
     public IActionResult Delete(int id)
     {
         // delete the selected car from Cars
         Cars.RemoveAt(id);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(string newCarName)
+    {
+        Cars.Add(newCarName);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        // retrieve the car with the specified index
+        // send both the id & name of the specified car to the edit form using ViewBag
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, string updatedCarName)
+    {
+        // update the car name of the car of the specified id 
         return RedirectToAction("Index");
     }
 }
